57f0332b7555f8f39eba3f213f1810a5dfb4462c
Manon971 <72388915+Manon971@users.noreply.github.com>
2024-02-12 17:40:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,15 +22,12 @@ def meteo():
     json_content = json.loads(raw_content.decode('utf-8'))
     results = []
     for list_element in json_content.get('list', []):
-      dt_txt = list_element.get('dt_txt')  # Date et heure de la prévision
-      temp_k = list_element.get('main', {}).get('temp')  # Température en Kelvin
-    
-      #if temp_k is not None:  # Vérifiez que la température est bien présente
-      temp_c = temp_k - 273.15  # Conversion de Kelvin en Celsius
-      results.append({'Date': dt_txt, 'Température (°C)': temp_c})
-
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('temp', {}).get('dt_txt') - 273.15 # Conversion de Kelvin en °c 
+        temp_day_value = list_element.get('temp', {}).get('dt_txt', {}) - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
 return jsonify(results=results)
-
+ 
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
