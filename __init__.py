ddfd91d8ca103b855bfbf35ddb7c00d71433f664
Manon971 <72388915+Manon971@users.noreply.github.com>
2024-02-12 18:42:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -36,7 +36,7 @@ def histogramme():
 
 @app.route('/extract-minutes/<date_string>')
 def extract_minutes(date_string):
-        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        date_object = datetime.strptime(date_string, '2024-02-11T11:57:27Z')
         minutes = date_object.minute
         return jsonify({'minutes': minutes})
   
